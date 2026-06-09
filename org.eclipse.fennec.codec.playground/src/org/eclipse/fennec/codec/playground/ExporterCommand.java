fb809e5c556c0b8ca3f34eeba147ce90234f464f
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2026-06-09 05:58:14 +0200

    codec playground bundle
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

---
+++ org.eclipse.fennec.codec.playground/src/org/eclipse/fennec/codec/playground/ExporterCommand.java
@@ -0,0 +1,119 @@
+package org.eclipse.fennec.codec.playground;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.felix.service.command.Descriptor;
+import org.apache.felix.service.command.annotations.GogoCommand;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.fennec.codec.tabular.CodecTabularOptions;
+import org.eclipse.fennec.codec.tabular.model.tabular.ReferenceMode;
+import org.gecko.emf.osgi.example.model.basic.Address;
+import org.gecko.emf.osgi.example.model.basic.BasicFactory;
+import org.gecko.emf.osgi.example.model.basic.BasicPackage;
+import org.gecko.emf.osgi.example.model.basic.Contact;
+import org.gecko.emf.osgi.example.model.basic.ContactContextType;
+import org.gecko.emf.osgi.example.model.basic.ContactType;
+import org.gecko.emf.osgi.example.model.basic.Person;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+@Component(service = ExporterCommand.class, scope = ServiceScope.PROTOTYPE)
+@GogoCommand(scope = "exporter", function = "export")
+public class ExporterCommand {
+
+	@Reference
+	BasicPackage basicPackage;
+
+	@Reference
+	private ResourceSet resourceSet;
+
+	private static final Path TEMP_FOLDER = Path.of(System.getProperty("java.io.tmpdir"));
+
+	@Descriptor("Export a test object in a resource with the provided URI")
+	public void export(
+			@Descriptor("The type of the export. Could be CSV, ODS, XLSX or R") 
+			String type, 
+			@Descriptor("The mode for the exporter. Could be IGNORE, FLAT, SQL_TABLES")
+			String mode
+			) throws IOException {
+
+		URI uri = null;
+		ReferenceMode referenceMode = ReferenceMode.valueOf(mode);
+		Map<String, Object> effective = new HashMap<>();
+		effective.putIfAbsent(CodecTabularOptions.OPTION_REFERENCE_MODE,
+				ReferenceMode.valueOf(mode));
+		switch(type) {
+		case "CSV":
+			if(ReferenceMode.SQL_TABLES.equals(referenceMode)) {
+				uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".csvz")).toString());
+			} else {
+				uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".csv")).toString());
+			}
+			break;
+		case "ODS":
+			uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".ods")).toString());
+			break;
+		case "XLSX":
+			uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".xlsx")).toString());
+			break;
+		case "R":
+			if(ReferenceMode.SQL_TABLES.equals(referenceMode)) {
+				uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".rdataz")).toString());
+				effective.put("codec.rlang.dataframePerFile", true);
+			} else {
+				uri = URI.createURI(TEMP_FOLDER.resolve(UUID.randomUUID().toString().concat(".RData")).toString());
+			}
+		default:
+			System.err.println(String.format("Exporter type %s not supported!", type));
+		}
+
+		Resource resource = resourceSet.createResource(uri);
+		Person person1 = createPerson("John", "Doe");
+		Person person2 = createPerson("Mario", "Rossi");
+		resource.getContents().add(person1);
+		resource.getContents().add(person2);
+		
+		try {
+			resource.save(effective);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(String.format("Result saved in %s", uri.toString()));
+
+	}
+
+	private Person createPerson(String firstName, String lastName) {
+		Person person = BasicFactory.eINSTANCE.createPerson();
+		person.setId(UUID.randomUUID().toString());
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		person.setAddress(createAddress());
+		person.getContact().add(createContact(ContactContextType.HOME, ContactType.MOBILE, "123456677"));
+		person.getContact().add(createContact(ContactContextType.PRIVATE, ContactType.EMAIL, "i.salforr@gmail.com"));
+		return person;
+	}
+
+	private Address createAddress() {
+		Address address = BasicFactory.eINSTANCE.createAddress();
+		address.setCity("Belluno");
+		address.setId(UUID.randomUUID().toString());
+		address.setZip("32037");
+		return address;
+	}
+
+	private Contact createContact(ContactContextType contextType, ContactType type, String value) {
+		Contact contact = BasicFactory.eINSTANCE.createContact();
+		contact.setContext(contextType);
+		contact.setType(type);
+		contact.setValue(value);
+		return contact;
+	}
+
+}
---
+++ org.eclipse.fennec.codec.playground/src/org/eclipse/fennec/codec/playground/ExporterResource.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2012 - 2026 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+package org.eclipse.fennec.codec.playground;
+
+
+
+import org.eclipse.emf.ecore.EObject;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.jakartars.whiteboard.propertytypes.JakartarsName;
+import org.osgi.service.jakartars.whiteboard.propertytypes.JakartarsResource;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+/**
+ * 
+ * @author ilenia
+ * @since Jun 3, 2026
+ */
+@JakartarsResource()
+@JakartarsName("ExporterResource")
+@Component(name = "ExporterResource", service = ExporterResource.class, scope = ServiceScope.PROTOTYPE)
+@Path("/export")
+public class ExporterResource {
+	
+	@GET
+	public Response hello() {
+		return Response.ok("Hello Exporter Resource").build();
+	}
+	
+	@POST
+	@Consumes("application/json")
+	@Produces({
+	      "text/csv",
+	      "application/x-csv-zip",
+	      "application/vnd.oasis.opendocument.spreadsheet",
+	      "application/x-rdata",
+	      "application/x-rdata-zip",
+	      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	  })
+
+    public Response export(EObject eObject) {
+		return Response.ok(eObject)
+		          .header("Content-Disposition", "attachment; filename=\"export\"")
+		          .build();
+	}
+}
